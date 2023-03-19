e5f91478001e39326ea10181da341f673ae52d33
infinex-exchange <admin@vayamos.cc>
2023-03-19 13:17:24 +0100

    Derive colors

--- account/affiliate.js
+++ account/affiliate.js
@@ -562,8 +562,8 @@ function showEarnDetails(month, year, refid) {
                     data: serieData
                 });
                 
-                console.log(deriveColor(dictRewardTypeColor[rtype], (lvl - 1) * 40));
-                colors.push(deriveColor(dictRewardTypeColor[rtype], (lvl - 1) * 40));
+                console.log(deriveColor(dictRewardTypeColor[rtype], (lvl - 1) * 20));
+                colors.push(deriveColor(dictRewardTypeColor[rtype], (lvl - 1) * 20));
                 
                 if(serieCount > serieMaxCount)
                     serieMaxCount = serieCount;

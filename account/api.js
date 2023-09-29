ebfe1055e0add76c956d08d6931bd8238eaa6c87
infinex-exchange <admin@infinex.cc>
2023-09-29 15:13:49 +0200

    V2 api keys

--- account/api.js
+++ account/api.js
@@ -1,5 +1,3 @@
-window.renderingStagesTarget = 1;
-
 $(document).ready(function() {
     $('#api-key-description').on('input', function() {
         if(validateApiKeyDescription($(this).val()))
@@ -9,44 +7,39 @@ $(document).ready(function() {
     });
 });
 
+function renderApiKey(data) {
+    return `
+        <div class="api-keys-item row p-2 hoverable" onClick="mobileApiKeyDetails(this)"
+         data-sid="${data.sid}" data-api-key="${data.apiKey}" data-description="${data.description}">
+            <div class="col-12 col-lg-4 my-auto wrap">
+                <h5 class="secondary api-key-description d-lg-none">${data.description}</h5>
+                <span class="api-key-description d-none d-lg-inline">${data.description}</span>
+            </div>
+            <div class="col-12 col-lg-5">
+                <div class="row flex-nowrap">
+                    <div class="col-auto col-lg-10 my-auto wrap">
+                        <span class="wrap" id="api-key-${data.sid}">${data.apiKey}</span>
+                    </div>
+                    <div class="col-auto col-lg-2 my-auto">
+                        <a href="#_" class="secondary" data-copy="#api-key-${data.sid}" onClick="copyButton(this); event.stopPropagation();"><i class="fa-solid fa-copy fa-xl"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3 d-none d-lg-block my-auto">
+                <button type="button" class="btn btn-primary btn-sm" onClick="showEditAKPrompt(${data.sid})">Rename</a>
+                <button type="button" class="btn btn-primary btn-sm" onClick="removeAK(${data.sid})">Remove</a>
+            </div>
+        </div>
+    `;      
+}
+
 function removeAK(sid) {
     api(
         'DELETE',
         '/account/api-keys/' + sid
     ).then(function() {
-        $('.sessions-item[data-sid=' + sid + ']').remove();
+        window.apiKeysScr.reset();
     });
-}
-
-function addChangeApiKey(sid, api_key, description) {
-    let elem = $('.sessions-item[data-sid=' + sid + ']');
-    if(elem.length) {
-        elem.data('description', description);
-        elem.find('.api-key-description').html(description);
-    }
-    else $('#api-keys-data').append(`
-        <div class="sessions-item row p-2 hoverable" onClick="mobileApiKeyDetails(this)"
-         data-sid="${sid}" data-api-key="${api_key}" data-description="${description}">
-            <div class="col-12 col-lg-4 my-auto wrap">
-                <h5 class="secondary api-key-description d-lg-none">${description}</h5>
-                <span class="api-key-description d-none d-lg-inline">${description}</span>
-            </div>
-            <div class="col-12 col-lg-5">
-                <div class="row flex-nowrap">
-                    <div class="col-auto col-lg-10 my-auto wrap">
-                        <span class="wrap" id="api-key-${sid}">${api_key}</span>
-                    </div>
-                    <div class="col-auto col-lg-2 my-auto">
-                        <a href="#_" class="secondary" data-copy="#api-key-${sid}" onClick="copyButton(this); event.stopPropagation();"><i class="fa-solid fa-copy fa-xl"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-3 d-none d-lg-block my-auto">
-                <button type="button" class="btn btn-primary btn-sm" onClick="showEditAKPrompt(${sid})">Rename</a>
-                <button type="button" class="btn btn-primary btn-sm" onClick="removeAK(${sid})">Remove</a>
-            </div>
-        </div>
-    `);      
 }
 
 function showAddAKPrompt() {
@@ -70,7 +63,7 @@ function showAddAKPrompt() {
                 description: description
             }
         ).then(function(data) {
-            addChangeApiKey(data.sid, data.apiKey, description);
+            window.apiKeysScr.reset();
         });
     });
     
@@ -102,7 +95,7 @@ function showEditAKPrompt(sid) {
                 description: description
             }
         ).then(function() {
-            addChangeApiKey(sid, null, description);
+            window.apiKeysScr.reset();
         });   
     });
     
@@ -115,32 +108,12 @@ $(document).on('authChecked', function() {
     if(!window.loggedIn)
         return;
     
-    //
-        $.ajax({
-            url: config.apiUrl + '/account/api_keys/list',
-            type: 'POST',
-            data: JSON.stringify({
-                api_key: window.apiKey
-            }),
-            contentType: "application/json",
-            dataType: "json",
-        })
-        .retry(config.retry)
-        .done(function (data) {
-            if(data.success) {
-                $.each(data.sessions, function(sid, v) {
-                    addChangeApiKey(sid, v.api_key, v.description); 
-                });
-                        
-                $(document).trigger('renderingStage');
-            } else {
-                msgBoxRedirect(data.error);
-            }
-        })
-        .fail(function (jqXHR, textStatus, errorThrown) {
-            msgBoxNoConn(true);
-        });     
-    }
+    window.apiKeysScr = new InfiniteScrollOffsetPg(
+        '/account/api-keys',
+        'apiKeys',
+        renderApiKey,
+        '#api-keys-data'
+    );
 });
 
 function mobileApiKeyDetails(item) {

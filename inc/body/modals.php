12fbfaa73518851fd3666a87441914da9940c9f2
infinex-exchange <admin@infinex.cc>
2023-09-29 16:07:18 +0200

    Hide modal fix

--- inc/body/modals.php
+++ inc/body/modals.php
@@ -24,7 +24,7 @@
             <div class="modal-body">
             </div>
             <div class="modal-footer">
-                <button type="button" class="yes btn btn-primary">Yes</button>
+                <button type="button" class="modal-close yes btn btn-primary" data-bs-dismiss="modal">Yes</button>
                 <button type="button" class="modal-close btn btn-secondary" data-bs-dismiss="modal">No</button>
             </div>
         </div>

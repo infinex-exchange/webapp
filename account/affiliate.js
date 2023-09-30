7aed33be406b2b3fb9bdc10f2ec62408718db0a4
infinex-exchange <admin@infinex.cc>
2023-09-30 17:26:22 +0200

    Debug

--- account/affiliate.js
+++ account/affiliate.js
@@ -500,7 +500,6 @@ function showRewards(refid, afseid) {
         }
         
         chart.updateSeries(series);
-        console.log(series);
         chart.updateOptions({
             chart: {
                 height: serieMaxCount * 35

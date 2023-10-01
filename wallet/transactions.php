e3399c7f3897f73c0719ac49f38fb757d498d6b1
infinex-exchange <admin@infinex.cc>
2023-10-01 13:08:30 +0200

    transactions

--- wallet/transactions.php
+++ wallet/transactions.php
@@ -47,6 +47,8 @@
         </div>
         </div>
         
+        <script src="/wallet/transactions.js?<?php echo filemtime(__DIR__.'/transactions.js'); ?>"></script>
+        
         <?php
         include(__DIR__.'/inc/tx_history.php');
         include(__DIR__.'/../inc/footer.php');

93f2152ec0c953c0cd060e2136968e59ccc6d845
infinex-exchange <admin@infinex.cc>
2023-10-04 11:34:36 +0200

    Deposit

--- wallet/deposit.js
+++ wallet/deposit.js
@@ -1,105 +1,91 @@
-$(document).ready(function() {
-    window.renderingStagesTarget = 2;
+function onCoinSelected(symbol) {
+    $('#deposit-step2').hide();
+    $('#deposit-step3').hide();
+    window.selectNet.reset('/wallet/v2/assets/' + symbol + '/networks');
+}
+
+function onNetSelected(symbol) {
+    $('#deposit-step3').hide();
     
-    $('#select-coin').on('dataLoaded', function() {
-        $(document).trigger('renderingStage');
-    });
-    
-    $('#select-coin').on('change', function() {
-        $('#deposit-step2').hide();
-        $('#deposit-step3').hide();
-        initSelectNet( $('#select-coin').val() );
-    });
-    
-    $('#select-net').on('dataLoaded', function() {
-        $('#deposit-step2').show();
-    });
-    
-    $('#select-net').on('change', function() {
-        $('#deposit-step3').hide();
-        
-        $.ajax({
-            url: config.apiUrl + '/wallet/deposit',
-            type: 'POST',
-            data: JSON.stringify({
-                api_key: window.apiKey,
-                asset: $('#select-coin').val(),
-                network: $('#select-net').data('network')
-            }),
-            contentType: "application/json",
-            dataType: "json",
-        })
-        .retry(config.retry)
-        .done(function (data) {
-            if(data.success) {
-                // Operating warning
-                if(data.operating)
-                    $('#deposit-operating-warning').addClass('d-none');
-                else
-                    $('#deposit-operating-warning').removeClass('d-none');
-                
-                // Experimental warning
-                if(!data.experimental)
-                    $('#deposit-experimental-warning').addClass('d-none');
-                else
-                    $('#deposit-experimental-warning').removeClass('d-none');
-                
-                // Warning
-                if(typeof(data.warning) !== 'undefined') {
-                    $('#deposit-warning-content').html(data.warning);
-                    $('#deposit-warning').removeClass('d-none')
-                }
-                else
-                    $('#deposit-warning').addClass('d-none');
-                
-                // Confirms target
-                $('#deposit-confirmations').html(data.confirms_target);
-                
-                // Address
-                $('#deposit-addr').html(data.address);
-                
-                // QR
-                if(typeof(data.qr_content) !== 'undefined') {
-                    window.qrcode.clear();
-                    window.qrcode.makeCode(data.qr_content);
-                    $('#deposit-qr-wrapper').removeClass('d-none');
-                }
-                else
-                    $('#deposit-qr-wrapper').addClass('d-none');
-                
-                // Memo
-                if(typeof(data.memo_name) !== 'undefined' && typeof(data.memo) !== 'undefined') {
-                    $('#deposit-memo-name').html(data.memo_name);
-                    $('#deposit-memo').html(data.memo);
-                    $('#deposit-memo-wrapper').removeClass('d-none');
-                }
-                else
-                    $('#deposit-memo-wrapper').addClass('d-none');
-                
-                // Contract
-                if(typeof(data.contract) !== 'undefined') {
-                    $('#deposit-contract').html(data.contract);
-                    $('#deposit-contract-wrapper').removeClass('d-none');
-                }
-                else {
-                    $('#deposit-contract-wrapper').addClass('d-none');
-                }
-        
-                $('#deposit-step3').show();
-                $('html, body').animate({
-                    scrollTop: $("#deposit-step3").offset().top
-                }, 1000);
-            } else {
-                msgBox(data.error);
+    $.ajax({
+        url: config.apiUrl + '/wallet/deposit',
+        type: 'POST',
+        data: JSON.stringify({
+            api_key: window.apiKey,
+            asset: $('#select-coin').val(),
+            network: $('#select-net').data('network')
+        }),
+        contentType: "application/json",
+        dataType: "json",
+    })
+    .retry(config.retry)
+    .done(function (data) {
+        if(data.success) {
+            // Operating warning
+            if(data.operating)
+                $('#deposit-operating-warning').addClass('d-none');
+            else
+                $('#deposit-operating-warning').removeClass('d-none');
+            
+            // Experimental warning
+            if(!data.experimental)
+                $('#deposit-experimental-warning').addClass('d-none');
+            else
+                $('#deposit-experimental-warning').removeClass('d-none');
+            
+            // Warning
+            if(typeof(data.warning) !== 'undefined') {
+                $('#deposit-warning-content').html(data.warning);
+                $('#deposit-warning').removeClass('d-none')
             }
-        })
-        .fail(function (jqXHR, textStatus, errorThrown) {
-            msgBoxNoConn(false);
-        });
-    });
+            else
+                $('#deposit-warning').addClass('d-none');
+            
+            // Confirms target
+            $('#deposit-confirmations').html(data.confirms_target);
+            
+            // Address
+            $('#deposit-addr').html(data.address);
+            
+            // QR
+            if(typeof(data.qr_content) !== 'undefined') {
+                window.qrcode.clear();
+                window.qrcode.makeCode(data.qr_content);
+                $('#deposit-qr-wrapper').removeClass('d-none');
+            }
+            else
+                $('#deposit-qr-wrapper').addClass('d-none');
+            
+            // Memo
+            if(typeof(data.memo_name) !== 'undefined' && typeof(data.memo) !== 'undefined') {
+                $('#deposit-memo-name').html(data.memo_name);
+                $('#deposit-memo').html(data.memo);
+                $('#deposit-memo-wrapper').removeClass('d-none');
+            }
+            else
+                $('#deposit-memo-wrapper').addClass('d-none');
+            
+            // Contract
+            if(typeof(data.contract) !== 'undefined') {
+                $('#deposit-contract').html(data.contract);
+                $('#deposit-contract-wrapper').removeClass('d-none');
+            }
+            else {
+                $('#deposit-contract-wrapper').addClass('d-none');
+            }
     
-    initSelectCoin();
-});
+            $('#deposit-step3').show();
+            $('html, body').animate({
+                scrollTop: $("#deposit-step3").offset().top
+            }, 1000);
+        } else {
+            msgBox(data.error);
+        }
+    })
+    .fail(function (jqXHR, textStatus, errorThrown) {
+        msgBoxNoConn(false);
+    });
+}
 
 $(document).on('authChecked', function() {
     if(!window.loggedIn)
@@ -112,9 +98,13 @@ $(document).on('authChecked', function() {
     window.selectCoin = new SelectCoin(
         $('#select-coin'),
         '/wallet/v2/assets',
-        function() {
-            alert('changed');
-        }
+        onCoinSelected
+    );
+    
+    window.selectNet = new SelectNet(
+        $('#select-net'),
+        null,
+        onNetSelected
     );
         
     let pathArray = window.location.pathname.split('/');

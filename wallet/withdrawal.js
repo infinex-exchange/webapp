dbf48de679d6ca7fcefd8628b01811fef5bdb8c2
infinex-exchange <admin@infinex.cc>
2023-10-09 13:59:37 +0200

    Dark amount

--- wallet/withdrawal.js
+++ wallet/withdrawal.js
@@ -22,7 +22,7 @@ function onCoinSelected(symbol) {
             
             $('#withdraw-balance').html(
                 darkBalance(
-                    window.bnAvbl,
+                    data.avbl,
                     window.paAmount.prec
                 )
             );

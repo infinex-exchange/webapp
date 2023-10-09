f54c3f7a956191d3ff4ffd6f6e098f456194c3fc
infinex-exchange <admin@infinex.cc>
2023-10-09 13:57:08 +0200

    Dark amount

--- js/global_utils.js
+++ js/global_utils.js
@@ -32,7 +32,7 @@ function prettyBalance(amount, prec) {
 function darkBalance(amount, prec) {
     let bn = new BigNumber(amount);
     let strAmountInPrec = bn.toFixed(prec);
-    let html = strAmountInPrec + '<span class="secondary">'
+    let html = strAmountInPrec + '<span style="opacity: 20%">'
     html += amount.substring(strAmountInPrec.length);
     html += '</span>';
     return html;

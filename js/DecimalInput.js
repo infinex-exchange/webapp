bc72d7097d802a6014d8497c7a2d1dab7a73cf60
infinex-exchange <admin@infinex.cc>
2023-10-09 12:09:11 +0200

    Syntax

--- js/DecimalInput.js
+++ js/DecimalInput.js
@@ -64,7 +64,7 @@ class DecimalInput {
             this.updateVTS();
         
         for(const callback of this.cbOnChangeUnchained)
-            callback(th.valReal);
+            callback(this.valReal);
     }
     
     onChange(callback, chained) {

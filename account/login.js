110caa7c65ec2eb92601f5f325aada4f60a7bc2f
infinex-exchange <admin@infinex.cc>
2023-10-10 13:01:26 +0200

    login form logic

--- account/login.js
+++ account/login.js
@@ -2,49 +2,31 @@ $(document).ready(function() {
     fvLogin = new FormValidator(
         $('#login-form'),
         function(data) {
-            console.log(data);
-            alert(data);
+            data.email = data.email.toLowerCase();
+            data['remember'] = $('#login-remember').prop('checked');
+            
+            api2fa(
+                'POST',
+                '/account/v2/sessions',
+                data
+            ).then(function (resp) {
+                sessionStorage.setItem('apiKey', resp.apiKey);
+                sessionStorage.setItem('userName', data.email);
+            
+                if(remember) {
+                    localStorage.setItem('_apiKey', resp.apiKey);
+                    localStorage.setItem('_userName', data.email);
+                }
+            
+                let redirectUrl = '/';
+                let urlParams = new URLSearchParams(window.location.search);
+                let back = urlParams.get('back');
+                if(back != null)
+                    redirectUrl = window.location.origin + back;
+                window.location.replace(redirectUrl);
+            });
         }
     );
     fvLogin.text('email', $('#login-email'), true, validateEmail);
     fvLogin.text('password', $('#login-password'), true, validatePassword);
-    /*$('#login-form').submit(function(event) {
-        event.preventDefault();
-        
-        let email = $('#login-email').val().toLowerCase();
-        let password = $('#login-password').val();
-        let remember = $('#login-remember').prop('checked');
-        
-        if(!validateEmail(email) ||
-           !validatePassword(password)
-        ) {
-            msgBox('Fill the form correctly');
-            return;
-        }
-        
-        api2fa(
-            'POST',
-            '/account/v2/sessions',
-            {
-                email: email,
-                password: password,
-                remember: remember
-            }
-        ).then(function (data) {
-            sessionStorage.setItem('apiKey', data.apiKey);
-            sessionStorage.setItem('userName', email);
-        
-            if(remember) {
-                localStorage.setItem('_apiKey', data.apiKey);
-                localStorage.setItem('_userName', email);
-            }
-        
-            let redirectUrl = '/';
-            let urlParams = new URLSearchParams(window.location.search);
-            let back = urlParams.get('back');
-            if(back != null)
-                redirectUrl = window.location.origin + back;
-            window.location.replace(redirectUrl);
-        });
-    });*/ 
 });

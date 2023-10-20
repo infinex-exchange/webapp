cfe3b8eeee3b64c07072c33219f1b4dffadedb71
infinex-exchange <admin@infinex.cc>
2023-10-20 12:23:56 +0200

    Debug

--- wallet/js/tx_history.js
+++ wallet/js/tx_history.js
@@ -217,10 +217,8 @@ function initTxHistory(
         preloader,
         null,
         function() { // onFirstPageLoaded
-            if(limit) {
+            if(limit)
                 window.scrTransactions.freeze();
-                console.log('freezed');
-            }
             
             setInterval(
                 function() {

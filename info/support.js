7fda991f4ce4aab4a20969527af0a4277ca98a01
infinex-exchange <admin@infinex.cc>
2023-10-23 11:34:50 +0200

    V2 support

--- info/support.js
+++ info/support.js
@@ -1,4 +1,5 @@
 window.sdYes = false;
+window.swXid = null;
 
 function gotoStep(step) {
     $('.support-step').addClass('d-none');
@@ -15,6 +16,60 @@ function submitConfirmation() {
 
 function onCoinSelected(symbol) {
     window.selectNet.reset('/wallet/v2/io/networks?asset=' + symbol);
+}
+
+function renderWithdrawal(data) { 
+    let time = new Date(data.createTime * 1000).toLocaleString();
+    
+    return `
+        <div data-id="${data.xid}" class="col-12 col-md-6 col-lg-4 sw-trans-item hoverable px-1 py-2"
+         onClick="selectWithdrawal(this)">
+        <div class="row m-0">
+
+            <div style="width: 60px" class="my-auto p-2">
+                <img width="40" height="40" src="${data.asset.iconUrl}">
+            </div>
+            
+            <div style="width: calc(100% - 60px)" class="my-auto">
+                <small class="secondary">${time}</small>
+                <br>
+                ${data.amount} ${data.asset.symbol}
+                <br>
+                <small>${data.network.name}</small>
+            </div>
+            
+        </div>
+        </div>
+    `;
+}
+
+function selectWithdrawal(item) {
+    let data = $(item).data();
+    
+    if(data.status == 'CANCELED') {
+        gotoStep('support-withdrawal-canceled');
+        return;
+    }
+    if(data.status == 'PENDING' || data.status == 'PROCESSING') {
+        gotoStep('support-withdrawal-pending');
+        return;
+    }
+    
+    let orderTime = new Date(data.createTime * 1000);
+    let nowTime = new Date();
+    
+    let msBetweenDates = Math.abs(orderTime.getTime() - nowTime.getTime());
+    let hoursBetweenDates = msBetweenDates / (60 * 60 * 1000);
+    
+    if(hoursBetweenDates < 8) {    
+        gotoStep('support-withdrawal-lt8h');
+        return;
+    }
+    
+    window.swXid = data.xid;
+    
+    $('.sw-trans-item').not(item).remove();
+    $(item).removeClass('col-md-6 col-lg-4');
 }
 
 $(document).ready(function() {
@@ -145,135 +200,16 @@ $(document).on('authChecked', function() {
         function() { return true; }
     );
     
+    window.scrWithdrawals = new InfiniteScrollOffsetPg(
+        '/wallet/v2/io/transactions?type=WITHDRAWAL',
+        'transactions',
+        renderWithdrawal,
+        $('#sw-list')
+    );
+    
     $('#sd-yes').click(function() {
         window.sdYes = true;
         $('.sd-ynprompt').addClass('d-none');
         $('.sd-yes-answer').removeClass('d-none');
     });
 });
-
-/*
-
-$(document).on('authChecked', function() {
-    if(!window.loggedIn)
-        return;
-    
-    initSelectCoin();
-    
-    $.ajax({
-        url: config.apiUrl + '/wallet/transactions',
-        type: 'POST',
-        data: JSON.stringify({
-            api_key: window.apiKey,
-            offset: 0,
-            type: 'WITHDRAWAL'
-        }),
-        contentType: "application/json",
-        dataType: "json",
-    })
-    .retry(config.retry)
-    .done(function (data) {
-        if(data.success) {
-            var i = 0;
-
-            $.each(data.transactions, function(k, v) {
-                if(i > 19)
-                    return;
-
-                i++;
-                
-                $('#sw-list').append(renderWithdrawal(v));
-            });
-        }
-        else {
-            msgBoxRedirect(data.error);
-        }
-    })
-    .fail(function (jqXHR, textStatus, errorThrown) {
-        msgBoxNoConn(true);
-    });
-});
-
-function renderWithdrawal(data) { 
-    var unixTime = data.create_time * 1000;
-    var cTime = new Date(unixTime).toLocaleString();
-    
-    return `
-        <div class="col-12 col-md-6 col-lg-4 sw-trans-item hoverable px-1 py-2" data-xid="${data.xid}" data-time="${unixTime}"
-         data-assetid="${data.asset}" data-status="${data.status}" onClick="selectWithdrawal(this)">
-        <div class="row m-0">
-
-            <div style="width: 60px" class="my-auto p-2">
-                <img width="40" height="40" src="${data.icon_url}">
-            </div>
-            
-            <div style="width: calc(100% - 60px)" class="my-auto">
-                <small class="secondary">${cTime}</small>
-                <br>
-                ${data.amount} ${data.asset}
-                <br>
-                <small>${data.network_description}</small>
-            </div>
-            
-        </div>
-        </div>
-    `;
-}
-
-function selectWithdrawal(item) {
-    if(window.swXid !== null)
-        return;
-
-    var status = $(item).data('status');
-    if(status == 'CANCELED') {
-        gotoStep('support-withdrawal-canceled');
-        return;
-    }
-    if(status == 'PENDING' || status == 'PROCESSING') {
-        gotoStep('support-withdrawal-pending');
-        return;
-    }
-    
-    var then = new Date($(item).data('time'));
-    var now = new Date();
-    
-    var msBetweenDates = Math.abs(then.getTime() - now.getTime());
-    var hoursBetweenDates = msBetweenDates / (60 * 60 * 1000);
-    
-    if(hoursBetweenDates < 8) {    
-        gotoStep('support-withdrawal-lt8h');
-        return;
-    }
-    
-    var assetid = $(item).data('assetid');
-    
-    $.ajax({
-        url: config.apiUrl + '/wallet/assets',
-        type: 'POST',
-        data: JSON.stringify({
-            symbols: [ assetid ]
-        }),
-        contentType: "application/json",
-        dataType: "json",
-    })
-    .retry(config.retry)
-    .done(function (data) {
-        if(data.success) {
-            if(data.assets[assetid].experimental) {
-                gotoStep('support-experimental');
-                return;
-            }
-            
-            window.swXid = $(item).data('xid');
-            
-            $('.sw-trans-item').not(item).remove();
-            $(item).removeClass('col-md-6 col-lg-4');
-        }
-        else {
-            msgBox(data.error);
-        }
-    })
-    .fail(function (jqXHR, textStatus, errorThrown) {
-        msgBoxNoConn();
-    });
-}*/

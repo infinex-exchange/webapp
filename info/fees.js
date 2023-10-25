7a9402f964ec3906110c222ffb629be118ab4115
infinex-exchange <admin@infinex.cc>
2023-10-25 17:24:10 +0200

    V2 fees

--- info/fees.js
+++ info/fees.js
@@ -1,68 +1,36 @@
-$(document).ready(function() {
-    window.renderingStagesTarget = 2;
-    
-    $.ajax({
-        url: config.apiUrl + '/info/spot_fees',
-        type: 'POST',
-        data: JSON.stringify({
-        }),
-        contentType: "application/json",
-        dataType: "json",
-    })
-    .retry(config.retry)
-    .done(function (data) {
-        if(data.success) {
-            $.each(data.fees, function(level, data) {   
-                $('#spot-fees-data').append(`
-                    <div class="row p-2 hoverable">
-                        <div class="col-1 d-none d-lg-block">
-                            ${level}
-                        </div>
-                        <div class="col-1 d-lg-none my-auto text-center">
-                            Lvl
-                            <h3>${level}</h3>
-                        </div>
-                        <div class="col-11">
-                            <div class="row">
-                                <div class="col-6 d-lg-none secondary">
-                                    30d trade volume:
-                                </div>
-                                <div class="col-6 col-lg text-end">
-                                    &ge; ${data.volume} ${data.volume_asset}
-                                </div>
-                                <div class="col-6 d-lg-none secondary">
-                                    Hold:
-                                </div>
-                                <div class="col-6 col-lg text-end">
-                                    &ge; ${data.hold} ${data.hold_asset}
-                                </div>
-                                <div class="col-6 d-lg-none secondary">
-                                    Maker fee:
-                                </div>
-                                <div class="col-6 col-lg text-end">
-                                    ${data.maker_fee}%
-                                </div>
-                                <div class="col-6 d-lg-none secondary">
-                                    Taker fee:
-                                </div>
-                                <div class="col-6 col-lg text-end">
-                                    ${data.taker_fee}%
-                                </div>
-                            </div>
-                        </div>
+function renderWdFee(data) {
+    return `
+        <div data-id="${data.keyid}" class="api-keys-item row p-2 hoverable" onClick="showApiKey(this)"
+         data-api-key="${data.apiKey}" data-description="${data.description}">
+            <div class="col-12 col-lg-4 my-auto wrap">
+                <h5 class="secondary api-key-description d-lg-none">${data.description}</h5>
+                <span class="api-key-description d-none d-lg-inline">${data.description}</span>
+            </div>
+            <div class="col-12 col-lg-5">
+                <div class="row flex-nowrap">
+                    <div class="col-auto col-lg-10 my-auto wrap">
+                        <span class="wrap" id="api-key-${data.keyid}">${data.apiKey}</span>
                     </div>
-                `);
-            });
-            
-            $(document).trigger('renderingStage'); 
-        }
-        else {
-            msgBoxRedirect(data.error);
-        }
-    })
-    .fail(function (jqXHR, textStatus, errorThrown) {
-        msgBoxNoConn(true); 
-    });
+                    <div class="col-auto col-lg-2 my-auto">
+                        <a href="#_" class="secondary" data-copy="#api-key-${data.keyid}" onClick="copyButton(this); event.stopPropagation();"><i class="fa-solid fa-copy fa-xl"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3 d-none d-lg-block my-auto">
+                <button type="button" class="btn btn-primary btn-sm" onClick="event.stopPropagation(); editApiKey(${data.keyid})">Rename</a>
+                <button type="button" class="btn btn-primary btn-sm" onClick="event.stopPropagation(); removeApiKey(${data.keyid})">Remove</a>
+            </div>
+        </div>
+    `;      
+}
+
+$(document).ready(function() {
+    window.scrWdFees = new InfiniteScrollOffsetPg(
+        '/wallet/v2/io/fees',
+        'fees',
+        renderWdFee,
+        $('#withdrawal-fees-data')
+    );
     
     window.wdFeesAS = new AjaxScroll(
         $('#withdrawal-fees-data'),
@@ -136,5 +104,71 @@ $(document).ready(function() {
         },
         true,
         true
-    );        
+    );
+    
+    
+    
+    // TODO: legacy code
+    $.ajax({
+        url: config.apiUrl + '/info/spot_fees',
+        type: 'POST',
+        data: JSON.stringify({
+        }),
+        contentType: "application/json",
+        dataType: "json",
+    })
+    .retry(config.retry)
+    .done(function (data) {
+        if(data.success) {
+            $.each(data.fees, function(level, data) {   
+                $('#spot-fees-data').append(`
+                    <div class="row p-2 hoverable">
+                        <div class="col-1 d-none d-lg-block">
+                            ${level}
+                        </div>
+                        <div class="col-1 d-lg-none my-auto text-center">
+                            Lvl
+                            <h3>${level}</h3>
+                        </div>
+                        <div class="col-11">
+                            <div class="row">
+                                <div class="col-6 d-lg-none secondary">
+                                    30d trade volume:
+                                </div>
+                                <div class="col-6 col-lg text-end">
+                                    &ge; ${data.volume} ${data.volume_asset}
+                                </div>
+                                <div class="col-6 d-lg-none secondary">
+                                    Hold:
+                                </div>
+                                <div class="col-6 col-lg text-end">
+                                    &ge; ${data.hold} ${data.hold_asset}
+                                </div>
+                                <div class="col-6 d-lg-none secondary">
+                                    Maker fee:
+                                </div>
+                                <div class="col-6 col-lg text-end">
+                                    ${data.maker_fee}%
+                                </div>
+                                <div class="col-6 d-lg-none secondary">
+                                    Taker fee:
+                                </div>
+                                <div class="col-6 col-lg text-end">
+                                    ${data.taker_fee}%
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                `);
+            });
+            
+            $(document).trigger('renderingStage'); 
+        }
+        else {
+            msgBoxRedirect(data.error);
+        }
+    })
+    .fail(function (jqXHR, textStatus, errorThrown) {
+        msgBoxNoConn(true); 
+    });
 });
--- info/fees.php
+++ info/fees.php
@@ -1,24 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <?php include('../templates/head.php'); ?>
+        <?php include(__DIR__.'/../inc/head.php'); ?>
         <script src="/js/ajax_scroll.js?<?php echo filemtime(__DIR__.'/../js/ajax_scroll.js'); ?>"></script>
         <title>Fees | Infinex</title>
     </head>
     <body class="body-background">
-    
-        <!-- Preloader -->
-        <?php include('../templates/preloader.html'); ?>
-        
-        <!-- Navbar -->
-        <?php include('../templates/navbar.php'); ?>
+        <?php include(__DIR__.'/../inc/body.php'); ?>
         
         <!-- Root container -->
         <div id="root" class="container-fluid container-1500 p-0 background">
         <div class="row m-0 h-rest">
         
         <!-- Left column -->
-        <?php include('../templates/sidebar_info.php'); ?>
+        <?php include(__DIR__.'/inc/sidebar.php'); ?>
         
         <!-- Main column -->
         <div class="col-12 col-lg-9 p-0 ui-card ui-column">
@@ -69,15 +64,18 @@
             <div class="row p-2 ui-card-light">
             <div class="col-12">
             
-                <div class="row p-2 secondary">
+                <div class="row p-2 secondary d-none d-lg-block">
                     <div class="col">
                         <h5>Asset</h5>
                     </div>
                     <div class="col">
                         <h5>Network</h5>
                     </div>              
-                    <div class="col text-end d-none d-lg-block">
-                        <h5>Deposit fee</h5>
+                    <div class="col text-end">
+                        <h5>Min deposit</h5>
+                    </div>
+                    <div class="col text-end">
+                        <h5>Min withdrawal</h5>
                     </div>
                     <div class="col text-end">
                         <h5>Withdrawal fee</h5>
@@ -99,9 +97,10 @@
         
         <script src="/info/fees.js?<?php echo filemtime(__DIR__.'/fees.js'); ?>"></script>
         
-        <?php include('../templates/modals.php'); ?>
-        <?php include('../templates/footer.html'); ?>
-        <?php include('../templates/vanilla_mobile_nav.php'); ?>
+        <?php
+        include(__DIR__.'/../inc/footer.php');
+        include(__DIR__.'/../inc/vanilla_mobile_nav.php');
+        ?>
     
     </body>
 </html>
